01b94fe57ff155cc4bae414620178ae51e044557
Viilhov <133661770+Viilhov@users.noreply.github.com>
2023-06-15 16:50:15 +0200

    Added JS to insert real data from moment into Los Angeles and updated some HTML and CSS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-
+function updateTime() {
+  //Los Angeles//
+  let losangelesElement = document.querySelector("#los-angeles");
+  let losangelesDateElement = losangelesElement.querySelector(".date");
+  let losangelesTimeElement = losangelesElement.querySelector(".time");
+  losangelesDateElement.innerHTML = moment()
+    .tz("America/Los_Angeles")
+    .format("MMMM Do YYYY");
+  losangelesTimeElement.innerHTML = moment()
+    .tz("America/Los_Angeles")
+    .format("H:mm:ss [<small>]A[</small>]");
+}
+setInterval(updateTime, 1000);
